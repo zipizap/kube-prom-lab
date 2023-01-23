ee09a51acfaa3423bf2c3c61aa59609e646f1867
Paulo AC <zipizap123@gmail.com>
2023-01-24 00:49:21 +0100

    wip0

--- 0.create_cluster.sh
+++ 0.create_cluster.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -xefu
 
-# cr kind cluster and update kubeconfig
+
+# cr kind cluster with nginx-ingress 
 cat <<EOF >KindClusterConfig.yaml
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
@@ -27,20 +28,23 @@ nodes:
     hostPort: 10443
     protocol: TCP
 EOF
-
 # overwrites $KUBECONFIG or ~/.kube/config.yaml
 kind create cluster \
   --wait 5m \
   --config KindClusterConfig.yaml
-
-#install nginx ingress
+# install nginx ingress
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
   --timeout=90s
+cat <<EOT
+#############################################
+#  dockerHost:10080  -> k8s-ingress:80
+#  dockerHost: 10443 -> k8s-ingress:443
+#############################################
+EOT
 
 # save kubeconfig
 kind get kubeconfig > kind.kubeconfig.yaml
-
 source KUBECONFIG.source
